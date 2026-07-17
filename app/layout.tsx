8c977980356ba248e77697f44d0cd437ea5a92c9
negasva <nicovanegas23@gmail.com>
2026-07-17 11:50:16 -0500

    Refactor Meta Pixel tracking script
    Updated Meta Pixel script for tracking page views.

--- app/layout.tsx
+++ app/layout.tsx
@@ -147,9 +147,9 @@ export default function RootLayout({
           />
         )}
         {/* Meta Pixel */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
+  <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -158,10 +158,12 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '38101988956067213');
-fbq('track', 'PageView');`,
-          }}
-        />
-        <noscript>
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=38101988956067213&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
           <img
             height="1"
             width="1"
